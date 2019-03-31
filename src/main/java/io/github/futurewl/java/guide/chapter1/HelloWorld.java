09f18d747cb3ffcab796d689cc695f8482216a05
FutureWL <624263934@qq.com>
2019-03-31 23:43:44 +0800

    update chapter 1

---
+++ src/main/java/io/github/futurewl/java/guide/chapter1/HelloWorld.java
@@ -0,0 +1,22 @@
+package io.github.futurewl.java.guide.chapter1;
+
+/**
+ * 功能描述：Java 入门指南 我的第一个 Java 程序
+ *
+ * @author weilai create by 2019-03-31:22:34
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * 这是第一个Java程序
+     * 它将打印Hello World
+     * 这是一个多行注释的示例
+     */
+    public static void main(String[] args) {
+        // 这是一个单行注释
+        /* 这也是一个单行注释 */
+        System.out.println("Hello World!");
+    }
+
+}
