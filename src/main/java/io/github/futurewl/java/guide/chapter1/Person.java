4a8af094a6e2cb7798ca7fb844f17fd9fc0a2439
FutureWL <624263934@qq.com>
2019-04-09 10:39:01 +0800

    update chapter 2

---
+++ src/main/java/io/github/futurewl/java/guide/chapter1/Person.java
@@ -0,0 +1,29 @@
+package io.github.futurewl.java.guide.chapter1;
+
+/**
+ * 功能描述：变量描述
+ * <p>
+ * 变量类型|关键字 & 说明
+ * 访问控制|private protected public
+ * 类、方法和变量修饰符|abstract class extends final implements interface native new static strictfp synchronized transient volatile
+ * 程序控制语句|break case continue default do else for if instanceof return switch while
+ * 错误处理|assert catch finally throw throws try
+ * 包相关|import package
+ * 基本类型|boolean byte char double float int long short
+ * 变量引用|super this void
+ * 保留关键字|goto const null
+ *
+ * @author weilai create by 2019-04-01:08:44
+ * @version 1.0
+ */
+public class Person {
+    void hello() {
+        /*
+         * 局部变量
+         */
+        int name;
+    }
+}
+
+
+
